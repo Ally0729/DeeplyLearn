d8391dc87d5408f77ca7e17cd497c78e03d77bbb
zhangsaiping <1145178427@qq.com>
2016-03-29 16:29:38 +0800

    string class learning over

--- test2.cpp
+++ test2.cpp
@@ -29,9 +29,14 @@ int main(){
 using namespace std;
 int main(){
 	string str;
+	string::size_type strlen;
 	//getline(cin,str);遇到回车才会停止读入
 	cin>>str;//遇到空格就会停止读入
-	cout<<str.size()<<endl;//str.size()返回字符串的长度
+	strlen=str.size();//str.size()返回字符串的长度
+	//str.size()返回值是string::size_type类型，这是一种配套类型，为了适应不同的机器，它与unsigned int具有相同含义
+	//但是切记，要将该函数返回值赋给一个变量，它必须是string::size_type类型的，不能是int类型
+	//赋值给int类型还有一个问题，int类型数的范围太小，并不能保存太长的字符串，很容易溢出
+	cout<<strlen<<endl;
 }*/
 //2、empty()函数
 /*#include <iostream>
@@ -44,4 +49,28 @@ int main(){
 		cout<<"The string is empty"<<endl;
 	else cout<<str<<endl;
 	return 0;
+}*/
+//3、字符串的连接
+/*#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	string str1="Hello";
+	string str2="World!";
+	string str3=str1+str2;//correct   '+' means paste
+	//string str4="Hello"+"World"; error cannot add two pointers
+	string str5=str1+"Lucy";//corre  '+'两边至少有一个string类型的就可以
+	cout<<str3<<endl;
+}*/
+//4、字符串索引号须是string::size_type类型,尽管也许int也不会报错
+/*#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+	string::size_type idx;//取值范围0~str.size()-1
+	string str;
+	getline(cin,str);
+	for(idx=0;idx<str.size();idx++){//str.size()返回值是string::size_type类型，因此idx也需要是string::size_type类型
+		cout<<str[idx]<<endl;
+	}
 }*/
